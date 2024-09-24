77d2c5a3a9395a7c08b2bb48219c9a1c8a0b1680
Rishik Murpani <rishik.murpani@leadschool.in>
2024-09-24 19:32:30 +0530

    empty string test case

---
+++ test/stringCalculator.test.js
@@ -0,0 +1,6 @@
+describe('String calculator', () => {
+    test('returns 0 for an empty string', () => {
+        const calculator = new StringCalculator()
+        expect(calculator.add("").toBe(0))
+    })
+})
