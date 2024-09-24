152ad29816333855089b3158b059ac776bbc296c
Rishik Murpani <rishik.murpani@leadschool.in>
2024-09-24 20:22:27 +0530

    new line test case

--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -20,4 +20,9 @@ describe('String calculator', () => {
         const calculator = new StringCalculator()
         expect(calculator.add("10,20,30,40")).toBe(100)
     })
+
+    test('returns the sum of numbers having new lines as delimiter', () => {
+        const calculator = new StringCalculator()
+        expect(calculator.add("10\n20,30")).toBe(60)
+    })
 })
