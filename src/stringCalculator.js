503f223116922c1d1cb9a85d8817e6ad19eee185
Rishik Murpani <rishik.murpani@leadschool.in>
2024-09-25 01:20:48 +0530

    custom delimiters

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,8 +1,18 @@
 class StringCalculator {
+    /**
+     * @param {string} numbers 
+     * @returns {number}
+     */
     add(numbers) {
         if (numbers === "")
             return 0
-        const numberArr = numbers.split(/,|\n/)
+        let delimiter = /,|\n/;
+        if (numbers.startsWith("//")) {
+            const parts = numbers.split("\n");
+            delimiter = new RegExp(parts[0].slice(2));
+            numbers = parts[1];
+        }
+        const numberArr = numbers.split(delimiter)
         return numberArr.reduce((total, num) => total + parseInt(num), 0)
     }
 }
--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -25,4 +25,9 @@ describe('String calculator', () => {
         const calculator = new StringCalculator()
         expect(calculator.add("10\n20,30")).toBe(60)
     })
+
+    test('returns the sum of numbers having custom delimiters', () => {
+        const calculator = new StringCalculator()
+        expect(calculator.add("//;\n1;2")).toBe(3)
+    })
 })
