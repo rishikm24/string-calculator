0cd9b544e32cda2372d4cc273a511e287ccdbe39
Rishik Murpani <rishik.murpani@leadschool.in>
2024-09-24 19:40:20 +0530

    bracket test

--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -1,6 +1,8 @@
+const StringCalculator = require('../src/stringCalculator')
+
 describe('String calculator', () => {
     test('returns 0 for an empty string', () => {
         const calculator = new StringCalculator()
-        expect(calculator.add("").toBe(0))
+        expect(calculator.add("")).toBe(0)
     })
 })
