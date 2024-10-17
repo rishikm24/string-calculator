b2a804d655ad03cb903374137f585f2c41ac865a
Rishik Murpani <rishik.murpani@leadschool.in>
2024-10-17 12:34:29 +0530

    multiplication test case

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -3,25 +3,37 @@ class StringCalculator {
      * @param {string} numbers 
      * @returns {number}
      */
-    add(numbers) {
+    compute(numbers) {
+        let multiply = false
         if (numbers === "")
             return 0
         let delimiter = /,|\n/;
         if (numbers.startsWith("//")) {
             const parts = numbers.split("\n");
+            if (parts[0] == '//*') {
+                multiply = true
+                parts[0] = '//\\*'
+            }
             delimiter = new RegExp(parts[0].slice(2));
             numbers = parts[1];
         }
         const numberArr = numbers.split(delimiter).filter(o => o !== '')
         let negatives = []
-        const total = numberArr.reduce((total, num) => {
-            let value = parseInt(num)
-            if (value < 0) {
-                negatives.push(value)
+        let total = 0
+        if (multiply) {
+            for (let num of numberArr) {
+                total *= num
             }
-            if (value > 1000) value = 0
-            return total + value
-        }, 0)
+        } else {
+            total = numberArr.reduce((total, num) => {
+                let value = parseInt(num)
+                if (value < 0) {
+                    negatives.push(value)
+                }
+                if (value > 1000) value = 0
+                return total + value
+            }, 0)
+        }
         if (negatives.length) {
             throw new Error(`negative numbers not allowed: ${negatives.join(',')}`)
         }
--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -3,51 +3,56 @@ const StringCalculator = require('../src/stringCalculator')
 describe('String calculator', () => {
     test('returns 0 for an empty string', () => {
         const calculator = new StringCalculator()
-        expect(calculator.add("")).toBe(0)
+        expect(calculator.compute("")).toBe(0)
     })
 
     test('returns the value for a single number', () => {
         const calculator = new StringCalculator()
-        expect(calculator.add("1")).toBe(1)
+        expect(calculator.compute("1")).toBe(1)
     })
 
     test('returns the sum of two numbers', () => {
         const calculator = new StringCalculator()
-        expect(calculator.add("1,5")).toBe(6)
+        expect(calculator.compute("1,5")).toBe(6)
     })
 
     test('returns the sum of multiple numbers', () => {
         const calculator = new StringCalculator()
-        expect(calculator.add("10,20,30,40")).toBe(100)
+        expect(calculator.compute("10,20,30,40")).toBe(100)
     })
 
     test('returns the sum of numbers having new lines as delimiter', () => {
         const calculator = new StringCalculator()
-        expect(calculator.add("10\n20,30")).toBe(60)
+        expect(calculator.compute("10\n20,30")).toBe(60)
     })
 
     test('returns the sum of numbers having custom delimiters', () => {
         const calculator = new StringCalculator()
-        expect(calculator.add("//;\n1;2")).toBe(3)
+        expect(calculator.compute("//;\n1;2")).toBe(3)
     })
 
     test('throws an error when negative numbers are provided', () => {
         const calculator = new StringCalculator()
-        expect(() => calculator.add("1,-2,3")).toThrow('negative numbers not allowed: -2')
+        expect(() => calculator.compute("1,-2,3")).toThrow('negative numbers not allowed: -2')
     })
 
     test('throws an error when multiple negative numbers are provided', () => {
         const calculator = new StringCalculator()
-        expect(() => calculator.add("-1,-2,-3")).toThrow('negative numbers not allowed: -1,-2,-3')
+        expect(() => calculator.compute("-1,-2,-3")).toThrow('negative numbers not allowed: -1,-2,-3')
     })
 
     test('return the sum ignoring numbers greater than 1000', () => {
         const calculator = new StringCalculator()
-        expect(calculator.add("10,20,1001,30")).toBe(60)
+        expect(calculator.compute("10,20,1001,30")).toBe(60)
     })
 
     test('returns the sum of numbers having custom delimiters of any length', () => {
         const calculator = new StringCalculator()
-        expect(calculator.add("//[***]\n1***2***3")).toBe(6)
+        expect(calculator.compute("//[***]\n1***2***3")).toBe(6)
+    })
+
+    test('returns the product of numbers when the custom delimiter is an asterick', () => {
+        const calculator = new StringCalculator()
+        expect(calculator.compute("//*\n2*3")).toBe(6)
     })
 })
